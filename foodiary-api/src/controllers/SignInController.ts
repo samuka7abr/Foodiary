7bf109bbe526df5ac2b389f6db305f34c3c018b6
samuka7abr <samuelabrao2006@gmail.com>
2025-07-19 11:12:03 -0300

    Back-end: Data Type Validation by Zod

--- foodiary-api/src/controllers/SignInController.ts
+++ foodiary-api/src/controllers/SignInController.ts
@@ -1,9 +1,24 @@
 
 import { HttpRequest, HttpResponse } from '../types/Http';
-import { ok } from '../utils/http';
+import { badRequest, created, ok } from '../utils/http';
+import { z } from 'zod';
+
+
+const schema = z.object({
+  email: z.email(),
+  password: z.string().min(8),
+});
 
 export class SignInController {
-  static async handle(request: HttpRequest): Promise<HttpResponse> {
-    return ok({ accessToken: 'token de acesso' });
+  static async handle({ body }: HttpRequest): Promise<HttpResponse> {
+    const { success, error, data } = schema.safeParse(body);
+    
+    if(!success){
+      return badRequest({ errors: error.issues});
+    }
+
+    return created({
+      data,
+    });
   }
 }
