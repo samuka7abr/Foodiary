fec84a68886bdfb2f80304b1d7eaee2886185bf5
samuka7abr <samuelabrao2006@gmail.com>
2025-07-20 15:52:19 -0300

    Front-end: Feat: Sign In page

--- foodiary-app/src/contexts/AuthContext.tsx
+++ foodiary-app/src/contexts/AuthContext.tsx
@@ -1,5 +1,18 @@
 import { createContext } from 'react';
 
-export default function RootLayout(){
-    
+interface IAuthContextValue {
+  isLoggedIn: boolean;
+  isLoading: boolean;
+}
+
+export const AuthContext = createContext({} as IAuthContextValue);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn: false, isLoading: false }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
